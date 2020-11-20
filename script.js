5725cc54920eb016e30571d01f4fc3c783d1b93a
j6512 <justins8287@gmail.com>
2020-11-19 21:01:32 -0600

    finished

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(json) {
+            let container = document.getElementById("container");
+            let allAstronauts = "";
+        
+            for (astronaut of json) {
+                allAstronauts += 
+                    `<div class="astronaut">
+                        <div class="bio">
+                            <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                                <li>Active: ${astronaut.active}</li>
+                                <li>Skills: ${astronaut.skills.join(", ")}</li>
+                            </ul>
+                        </div>
+                        <img class="avatar" src="${astronaut.picture}">
+                    </div>`;
+               
+            }
+            container.innerHTML += allAstronauts;
+        });
+    });
+});
